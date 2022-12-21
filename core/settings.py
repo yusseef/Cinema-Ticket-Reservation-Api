a6ac759147ca386bb0e169a086e6e8434aced68f
Yussef raouf <yussef.raouf11@gmail.com>
2022-12-22 00:32:00 +0200

    SECRETKEEY

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-2la@c!h3(a!8a5y4_&r6ahom)j8-#!-63=djdqk8ib*(ocwwq8'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
